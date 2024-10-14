1169f6f4d9a64e64eae13f7120fbfffd18d78232
Manish Panda <pandamanish49@gmail.com>
2024-10-14 13:01:18 +0530

    test changes

--- src/classAndObjPractice/Cat.java
+++ src/classAndObjPractice/Cat.java
@@ -1,5 +1,11 @@
 package classAndObjPractice;
 
 public class Cat extends Animal{
+   void eat(){
+       System.out.println("cat eat fish");
+   }
 
+   void sound(){
+       System.out.println("cat sound like meow");
+   }
 }
